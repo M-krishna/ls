49d3e7ea9e491744013acd3a062fb7b0f65d5d7c
krishna M <krishnamurugan589@gmail.com>
2020-05-30 07:26:01 +0530

    (feature.) added count for total directories and files.

--- main.go
+++ main.go
@@ -71,11 +71,20 @@ func otherVersion(root string) error {
 		return err
 	}
 
+	var totalDirectory int = 0
+	var totalFiles int = 0
+
 	for _, info := range fis {
-		if info.Name()[0] != '.' {
+		if info.Name()[0] != '.' && info.Name()[0] != '$'{
+			if info.IsDir() {
+				totalDirectory += 1
+			} else {
+				totalFiles += 1
+			}
 			fmt.Printf(" %v\n", info.Name())
 		}
 	}
+	fmt.Fprint(os.Stdout, "Total Directory: ", totalDirectory, ", Total Files: ", totalFiles)
 
 	return nil
 }
